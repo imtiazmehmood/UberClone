cfa94de539b1c133e04968e46a7d460612d8493f
Imtiaz-Mahmood-Dev <imtiazdev@paklogics.com>
2025-07-18 17:50:15 +0500

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Apple on 18/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UberCloneTests/UberCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneTests.swift
+//  UberCloneTests
+//
+//  Created by Apple on 18/07/2025.
+//
+
+import Testing
+@testable import UberClone
+
+struct UberCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
