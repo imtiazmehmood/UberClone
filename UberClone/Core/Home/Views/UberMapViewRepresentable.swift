7e46d39b73e605aa22ada814e4fe49ed17061cc6
Imtiaz Mahmood <imtiazmehmood26@gmail.com>
2025-07-22 21:01:34 +0500

    clear  destination routes when no input

--- UberClone/Core/Home/Views/UberMapViewRepresentable.swift
+++ UberClone/Core/Home/Views/UberMapViewRepresentable.swift
@@ -6,6 +6,7 @@ import MapKit
 struct UberMapViewRepresentable: UIViewRepresentable {
     let mapView = MKMapView()
     let locationManager = LocationManager()
+    @Binding var mapState: MapViewState
     @EnvironmentObject var locationViewModel: LocationSearchViewModel
     
     func makeUIView(context: Context) -> some UIView {
@@ -18,11 +19,22 @@ struct UberMapViewRepresentable: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
-        if let coordinate = locationViewModel.selectedLocationCoordinate {
-            
-            context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
-            context.coordinator.configurePolyline(withDestinationCoordinate: coordinate)
+        
+        switch mapState {
+        case .noInput:
+            context.coordinator.clearMapViewAndRecenterOnUserLocation()
+            break
+        case .searchingForLocation:
+            break
+        case .locationSelected:
+            if let coordinate = locationViewModel.selectedLocationCoordinate {
+                
+                context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
+                context.coordinator.configurePolyline(withDestinationCoordinate: coordinate)
+            }
+            break
         }
+        
     }
     
     func makeCoordinator() -> MapCoordinator {
@@ -37,6 +49,7 @@ extension UberMapViewRepresentable {
         
         let parent : UberMapViewRepresentable
         var userLocationCoordinate: CLLocationCoordinate2D?
+        var currentRegion:MKCoordinateRegion?
         
         // MARK: - Lifecycle
         
@@ -53,6 +66,8 @@ extension UberMapViewRepresentable {
                 latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude),
                 span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
             )
+            
+            self.currentRegion = region
             
             parent.mapView.setRegion(region, animated: true)
         }
@@ -78,6 +93,7 @@ extension UberMapViewRepresentable {
         
         func configurePolyline(withDestinationCoordinate coordinate: CLLocationCoordinate2D) {
             guard let userLocationCoordinate = self.userLocationCoordinate else { return }
+            
             getDestinationRoute(from: userLocationCoordinate, to: coordinate) { route in
                 self.parent.mapView.addOverlay(route.polyline)
                 
@@ -100,6 +116,16 @@ extension UberMapViewRepresentable {
                 guard let route = response?.routes.first else { return }
                 completion(route)
             }
+        }
+        
+        func clearMapViewAndRecenterOnUserLocation(){
+            parent.mapView.removeAnnotations(parent.mapView.annotations)
+            parent.mapView.removeOverlays(parent.mapView.overlays)
+            
+            if let currentRegion = currentRegion {
+                parent.mapView.setRegion(currentRegion, animated: true)
+            }
+            
         }
     }
 }
---
+++ UberClone/Models/MapViewState.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewState.swift
+//  UberClone
+//
+//  Created by Apple on 19/07/2025.
+//
+
+import Foundation
+
+enum MapViewState {
+    case noInput
+    case locationSelected
+    case searchingForLocation
+}
